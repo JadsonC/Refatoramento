4e7fb6ec2a101810eaf6e3e37ea4cf1ffb0e3823
JadsonC <54084395+JadsonC@users.noreply.github.com>
2020-02-06 23:47:32 -0300

    Create LongMethodCorrigido.java

---
+++ LongMethodCorrigido.java
@@ -0,0 +1,50 @@
+
+package Model.DAO;
+
+import Model.Livro;
+import java.util.ArrayList;
+
+
+public class LivroDAO {
+  
+    public void insert(Livro livro){
+        Banco.livro.add(livro);
+        System.out.println(Banco.livro.size());
+    }
+    
+    public ArrayList<Livro> selectAll(){
+        return Banco.livro;
+    }
+    
+    public void limparTela(){
+        for(int i = 0; i < 300; i++) System.out.println("\n");
+    }
+    
+    public void mostrarNomeLivro(String nome){
+        limparTela();
+        for(int i = 0; i < Banco.livro.size(); i++) {
+            Livro livroLista = Banco.livro.get(i);
+            if(verificaNome(livroLista, nome)){
+                System.out.println(livroLista.tituloLivro + " - " + livroLista.autorLivro + "," + livroLista.ano);
+                break;
+            }
+        }
+    }
+    
+    public void mostrarLivroPorGenero(String genero){        
+        limparTela();
+        for(int i = 0; i < Banco.livro.size(); i++){
+            if(generosSaoIguais(Banco.livro.get(i), genero)){
+                System.out.println((Banco.livro.get(i).tituloLivro));
+            }
+        }        
+    }
+
+    public boolean generosSaoIguais(Livro livro, String genero){
+        return livro.generoLivro.equals(genero);
+    }
+    
+    public boolean verificaNome(Livro livro, String nome){
+        return livro.tituloLivro.equals(nome);
+    }
+}
